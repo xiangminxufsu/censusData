8665768be380c644315d5a2bed8d5c5cd213e86e
xiangminxufsu <GIS_tech@ISPA-T5400-X3.ad.freac.fsu.edu>
2015-09-11 13:49:37 -0400

    new code added and source1 deleted

--- _main.py
+++ _main.py
@@ -1,20 +1,37 @@
 #09/02/2015
 
 import os
+import time
 import zipfile
 from code.pre_process import pre_Process_main
 from code.step_one_merge_county import merge_main
-#from code.step_two_filter_ann import filter_main
-#from code.step_three_merge_table import merge_table_main
-
+from code.step_two_filter_ann import filter_main
+from code.step_three_merge_table import merge_table_main
+from code.unzip_filter import unziped_filter_main
+from code.clean import cleanFolder
 
 def ProcessZip(folder):
 	pre_Process_main(folder)
 	merge_main(folder)
-	#filter_main(folder)
-	#merge_table_main(folder)
+	
+	condition = raw_input("Do you want to change metadata in order to filter tables? (Y/N)  ")
+	if condition.lower() == 'y' or condition.lower() == 'yes':
+		print "Now please change the metadat..."
+		stopsign = None
+		while stopsign == None:
+			stopsign = raw_input("Press any key when you are ready...")
+			time.sleep(1)
+		print "Key is caught and start processing modified file..."
+	else:
+		time.sleep(1)
+		print "You don't want to change so nothing will be changed..."
+		
+	filter_main(folder)
+	merge_table_main(folder)
 	pass
 def ProcessUnzip(folder):
+	unziped_filter_main(folder)
+	merge_table_main(folder)
 	pass
 
 #finding folders
@@ -55,4 +72,5 @@ for x in os.listdir(inputPath):
 	else:
 		ProcessUnzip(folder)
 		print filePath,'no'
+	cleanFolder(folder,"Input","Output")
 	break
